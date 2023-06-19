37951d3eb46fafff1dab7654400d51fc2f6a9f3f
Brian Naughton <bpnaughton@yahoo.co.uk>
2023-06-19 20:37:16 +0100

    added cmetrics and utils

---
+++ pipelines/rawmetrics/java/src/main/java/free5gc/rawmetrics/CAdvisorMetric.java
@@ -0,0 +1,94 @@
+package free5gc.rawmetrics;
+
+import org.apache.beam.sdk.coders.DefaultCoder;
+import org.apache.beam.sdk.extensions.avro.coders.AvroCoder;
+
+@DefaultCoder(AvroCoder.class)
+public class CAdvisorMetric {
+    
+    String containerName;
+    Long timestamp= 0L;
+    Long memUsage = 0L;
+    Long cpuUsage = 0L;
+    Long rx_bytes = 0L;
+    Long rx_errors = 0L;
+    Long rx_dropped = 0L;
+    Long tx_bytes = 0L;
+    Long tx_errors = 0L;
+    Long tx_dropped = 0L;
+
+    public CAdvisorMetric() {}
+    
+    public CAdvisorMetric(String containerName, Long timestamp, Long memUsage, Long cpuUsage, Long rx_bytes, Long rx_errors, Long rx_dropped, Long tx_bytes, Long tx_errors, Long tx_dropped) {
+        this.containerName = containerName;
+        this.timestamp = timestamp;
+        this.memUsage = memUsage;
+        this.cpuUsage = cpuUsage;
+        this.rx_bytes = rx_bytes;
+        this.rx_errors = rx_errors;
+        this.rx_dropped = rx_dropped;
+        this.tx_bytes = tx_bytes;
+        this.tx_errors = tx_errors;
+        this.tx_dropped = tx_dropped;
+    }
+
+    public void setContainerName(String containerName) {
+        this.containerName = containerName;
+    }
+    public void setTimeStamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+    public void setMemUsage(Long memUsage) {
+        this.memUsage = memUsage;
+    }
+    public void setRx_bytes(Long rx_bytes) {
+        this.rx_bytes = rx_bytes;
+    }
+    public void setRx_errors(Long rx_errors) {
+        this.rx_errors = rx_errors;
+    }
+    public void setRx_dropped(Long rx_dropped) {
+        this.rx_dropped = rx_dropped;
+    }
+    public void setTx_bytes(Long tx_bytes) {
+        this.tx_bytes = tx_bytes;
+    }
+    public void setTx_errors(Long tx_errors) {
+        this.tx_errors = tx_errors;
+    }
+    public void setTx_dropped(Long tx_dropped) {
+        this.tx_dropped = tx_dropped;
+    }
+    
+    public String getContainerName() {
+        return containerName;
+    }    
+    public Long getTimeStamp() {
+        return timestamp;
+    }
+    public Long getMemUsage() {
+        return memUsage;
+    }
+    public Long getRx_bytes() {
+        return rx_bytes;
+    }
+    public Long getRx_errors() {
+        return rx_errors;
+    }
+    public Long getRx_dropped() {
+        return rx_dropped;
+    }
+    public Long getTx_bytes() {
+        return tx_bytes;
+    }
+    public Long getTx_errors() {
+        return tx_errors;
+    }
+    public Long getTx_dropped() {
+        return tx_dropped;
+    }
+    
+    public String toString() {
+        return String.format("containerName = %s, timestamp = %d, memUsage = %d, cpuUsage = %d, rx_bytes = %d, rx_errors = %d, rx_dropped = %d, tx_bytes = %d, tx_errors = %d, tx_dropped = %d", containerName, timestamp, memUsage, cpuUsage, rx_bytes, rx_errors, rx_dropped, tx_bytes, tx_errors, tx_dropped);
+    }
+}
--- pipelines/rawmetrics/java/src/main/java/free5gc/rawmetrics/RawMetrics.java
+++ pipelines/rawmetrics/java/src/main/java/free5gc/rawmetrics/RawMetrics.java
@@ -1,15 +1,29 @@
 package free5gc.rawmetrics;
+/*
+ * To change the runner, specify:
+ * --runner=YOUR_SELECTED_RUNNER
+ *
+ * To execute this pipeline, specify a local output file (if using the {@code DirectRunner}
+ * output prefix on a supported distributed file system.
+ *
+ * --output=[YOUR_LOCAL_FILE | YOUR_OUTPUT_PREFIX]
+ */
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.extensions.gcp.options.GcpOptions;
 import org.apache.beam.sdk.io.kafka.KafkaIO;
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.Validation.Required;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.SimpleFunction;
 import org.apache.beam.sdk.transforms.Values;
+import org.apache.beam.sdk.values.KV;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -18,46 +32,13 @@ import org.apache.beam.sdk.transforms.ParDo;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import java.time.Instant;
+
+import free5gc.rawmatrics.utils.WriteToBigQuery;
+import free5gc.rawmatrics.utils.WriteWindowedToBigQuery;
 
 public class RawMetrics {
     static ArrayList <String> containerList = new ArrayList<String>();
-
-    static class FormatMetrics extends DoFn<String, String> {
-        @ProcessElement
-        public void processElement(ProcessContext c) {
-            try{
-                // parse the json c.element() and extract the metrics we are interested in
-                JSONParser parser = new JSONParser(); 
-                JSONObject json = (JSONObject) parser.parse(c.element());
-
-                // search string for a list of container names
-                for (String element : containerList){
-                    if (element.contains(json.get("container_Name").toString())){
-                        System.out.printf("Container = %s\n", element);
-
-                        JSONObject stats = (JSONObject) json.get("container_stats");
-                        System.out.printf("timestamp = %s\n", stats.get("timestamp").toString());
-
-                        // report total cpu usage
-                        JSONObject cpu = (JSONObject) stats.get("cpu");
-                        JSONObject cpuUsage = (JSONObject) cpu.get("usage");
-                        System.out.printf("cpu = %s\n", cpuUsage.get("total").toString());
-
-                        // get memory usage
-
-                        // get network usage
-
-                        // get disk usage
-
-                    }
-                    c.output(c.element());
-                }
-
-            } catch (Exception e) {
-                System.out.printf("error %s \n\n\n", e);
-            }
-        }        
-    }
 
     private static void buildList(){
         containerList.add("webserver");
@@ -73,16 +54,84 @@ public class RawMetrics {
         containerList.add("ueransim");
     }
 
+    static class FormatMetrics extends DoFn<String, CAdvisorMetric> {
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+            try{
+                // parse the json c.element() and extract the metrics we are interested in
+                JSONParser parser = new JSONParser(); 
+                JSONObject json = (JSONObject) parser.parse(c.element());
+
+                // search string for a list of container names
+                for (String element : containerList){
+                    if (element.contains(json.get("container_Name").toString())){
+                        JSONObject stats = (JSONObject) json.get("container_stats");
+
+                        Instant now = Instant.now();
+                        Long timestamp = now.getEpochSecond();
+
+                        // report total cpu usage
+                        JSONObject cpu = (JSONObject) stats.get("cpu");
+                        JSONObject cpuUsage = (JSONObject)cpu.get("usage");
+
+                        // get memory usage
+                        JSONObject memory = (JSONObject) stats.get("memory");
+                        Long memUsage = (Long)memory.get("usage");
+
+                        // get network usage
+                        JSONObject network = (JSONObject) stats.get("network");
+                        Long rx_bytes = (Long) network.get("rx_bytes");
+                        Long rx_errors = (Long) network.get("rx_errors");
+                        Long rx_dropped = (Long) network.get("rx_dropped");
+                        Long tx_bytes = (Long) network.get("tx_bytes");
+                        Long tx_errors = (Long) network.get("tx_errors");
+                        Long tx_dropped = (Long) network.get("tx_dropped");
+
+                        CAdvisorMetric metric = new CAdvisorMetric(
+                            json.get("container_Name").toString(),
+                            timestamp,
+                            memUsage,
+                            (Long)cpuUsage.get("total"),
+                            rx_bytes,
+                            rx_errors,
+                            rx_dropped,
+                            tx_bytes,
+                            tx_errors,
+                            tx_dropped
+                        );
+
+                        c.output(metric);
+                    }
+                }
+
+            } catch (Exception e) {
+                System.out.printf("error %s \n\n\n", e);
+            }
+        }        
+    }
+
+    public interface RawMetricsOptions extends PipelineOptions {
+        @Description("Kafka Server Address")
+        @Default.String("10.211.55.3:29092")
+        String getKafkaServer();
+        void setKafkaServer(String value);
+
+        /** Set this required option to specify where to write the output. */
+        @Description("Path of the file to write to")
+        // @Required
+        String getOutput();
+        void setOutput(String value);
+    }
+
     public static void main(String[] args) {
         buildList();
 
-        PipelineOptions options =  PipelineOptionsFactory.create();
+        RawMetricsOptions options =  PipelineOptionsFactory.fromArgs(args).withValidation().as(RawMetricsOptions.class);
         final Pipeline p = Pipeline.create(options);
 
         p.apply(
             KafkaIO.<Long, String>read()
-                .withBootstrapServers(
-                    "10.211.55.3:29092") 
+                .withBootstrapServers(options.getKafkaServer()) 
                 .withTopicPartitions(
                     Collections.singletonList(
                         new TopicPartition(
@@ -92,17 +141,24 @@ public class RawMetrics {
                 .withValueDeserializer(StringDeserializer.class)
                 .withoutMetadata())
         .apply(Values.create())
-        .apply(ParDo.of(new FormatMetrics()));
+        .apply(ParDo.of(new FormatMetrics()))
+        .apply(
+            "PrintResults",
+            MapElements.via(
+                new SimpleFunction<CAdvisorMetric, CAdvisorMetric>() {
+                  @Override
+                  public CAdvisorMetric apply(CAdvisorMetric input) {
+                    System.out.printf(input.toString());
+                    return input;
+                  }
+                }));
         // .apply(
-        //     "FormatResults",
-        //     MapElements.via(
-        //         new SimpleFunction<String, String>() {
-        //           @Override
-        //           public String apply(String input) {
-        //             System.out.printf("value %s \n\n", input);
-        //             return input;
-        //           }
-        //         }));
+        //     "WriteTeamScoreSums",
+        //     new WriteWindowedToBigQuery<>(
+        //         options.as(GcpOptions.class).getProject(),
+        //         options.getDataset(),
+        //         options.getLeaderBoardTableName() + "_team",
+        //         configureWindowedTableWrite()));
 
         p.run().waitUntilFinish();    
     }    
